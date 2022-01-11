c01b8c9ae05ab13d5fdb56e992deceef8dbafa28
Sidney Andrews <v-sandrews@microsoft.com>
2022-01-11 14:42:10 +0000

    Initial commit

---
+++ script.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using Amazon.DynamoDBv2;
+
+MongoClient mongoClient = new ("mongodb://mongo:27017");
+Console.WriteLine("[Mongo]\tConnected");
+
+var databaseList = await mongoClient.ListDatabaseNamesAsync();
+
+Console.WriteLine("[Mongo]\tDatabases:");
+foreach (var database in await databaseList.ToListAsync<string>())
+{
+    Console.WriteLine($"- {database}");
+}
+
+
+
+AmazonDynamoDBConfig dynamodbConfig = new ()
+{
+    ServiceURL = "http://dynamodb:8000"
+};
+AmazonDynamoDBClient dynamodbClient = new (
+    awsAccessKeyId: $"{Guid.NewGuid()}",
+    awsSecretAccessKey: $"{Guid.NewGuid()}",
+    clientConfig: dynamodbConfig
+);
+Console.WriteLine("[DynamoDB]\tConnected");
+
+var tableList = await dynamodbClient.ListTablesAsync();
+
+Console.WriteLine("[DynamoDB]\tTables:");
+foreach (var table in tableList.TableNames)
+{
+    Console.WriteLine($"- {table}");
+}
